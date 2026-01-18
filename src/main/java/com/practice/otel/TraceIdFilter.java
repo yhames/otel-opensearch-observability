2014a9c3835d5e79610ed720d48bc8b9b6eb0002
yhames <wjddn494@naver.com>
2026-01-18 18:54:55 +0900

    Enhance OpenTelemetry setup with health checks and new pipelines for logs and metrics

---
+++ src/main/java/com/practice/otel/TraceIdFilter.java
@@ -0,0 +1,38 @@
+package com.practice.otel;
+
+import io.micrometer.tracing.TraceContext;
+import io.micrometer.tracing.Tracer;
+import jakarta.annotation.Nullable;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+class TraceIdFilter extends OncePerRequestFilter {
+
+    private final Tracer tracer;
+
+    TraceIdFilter(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String traceId = getTraceId();
+        if (traceId != null) {
+            response.setHeader("X-Trace-Id", traceId);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private @Nullable String getTraceId() {
+        TraceContext context = this.tracer.currentTraceContext().context();
+        return context != null ? context.traceId() : null;
+    }
+}
+
